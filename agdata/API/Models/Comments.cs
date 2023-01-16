ae72121d2923506908b1475d174c474306c4d4dd
Rachel <rlfuller@gmail.com>
2023-01-16 18:04:57 -0500

    Add models for api tests

---
+++ agdata/API/Models/Comments.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agdata.API.Models
+{
+    internal class Comments
+    {
+        [JsonProperty("postId")]
+        public int PostId { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("body")]
+        public string Body { get; set; }
+    }
+}
---
+++ agdata/API/Models/Posts.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agdata.API.Models
+{
+    internal class Posts
+    {
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set;  }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("body")]
+        public string Body { get; set;  }
+    }
+}
